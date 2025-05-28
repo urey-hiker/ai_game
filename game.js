88c74c3a600c7bcb8cdaa4c22c10466755a794bb
liqing <liqing@MAGIT00283.local>
2025-05-28 17:42:51 +0800

    fix: autoscale on safari

--- game.js
+++ game.js
@@ -1098,6 +1098,27 @@ function shareResult() {
     }
 }
 
+//阻止safari浏览器双击放大功能
+let lastTouchEnd = 0  //更新手指弹起的时间
+document.documentElement.addEventListener("touchstart", function (event) {
+    //多根手指同时按下屏幕，禁止默认行为
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+});
+document.documentElement.addEventListener("touchend", function (event) {
+    let now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+        //当两次手指弹起的时间小于300毫秒，认为双击屏幕行为
+        event.preventDefault();
+    }else{ // 否则重新手指弹起的时间
+        lastTouchEnd = now;
+    }
+}, false);
+//阻止双指放大页面
+document.documentElement.addEventListener("gesturestart", function (event) {
+    event.preventDefault();
+});
 // 初始化游戏
 document.addEventListener('DOMContentLoaded', initGame);
 // 根据选项数量调整容器的列数
